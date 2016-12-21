cd70dfd800a84ede2702ba9c82db49785344096b
Tarun Gupta <tarun1995gupta@gmail.com>
2016-12-22 03:44:50 +0530

    Finally correct

--- config.py
+++ config.py
@@ -1,16 +1,29 @@
 import numpy as np
 import mdptoolbox.example
 
-states = 2
+states = 4
 actions = 2
 num_of_var = states*actions
 #num_of_var = 3
-gamma = 0.95
-alpha = [0.5]*states
+gamma = 0.25
+a = float(1.0/float(states))
+print a
+alpha = [a]*states
 alpha = np.array(alpha)
 
-P, R = mdptoolbox.example.small()
-#P, R = mdptoolbox.example.forest()
+#P, R = mdptoolbox.example.small()
+P, R = mdptoolbox.example.forest(S=states,p=0.45)
+# print R
+
+# mask = np.zeros((states, states))
+# P, R = mdptoolbox.example.rand(states, actions, mask=mask)
+# newR = []
+# for x in R:
+#     for y in x:
+#         newR.append([sum(y)])
+# newR = np.array(newR)
+# R = newR
+
 tran = open('tdata1','w')
 rew = open('rdata1','w')
 for x in P:
@@ -19,12 +32,22 @@ for x in P:
         tran.write("\n")
     tran.write("\n")
 tran.close()
+mi = float('INF')
+ma = float('-INF')
 for x in R:
+    mini = min(x)
+    if mini < mi:
+        mi = mini
+
+    maxi = max(x)
+    if maxi > ma:
+        ma = maxi
+
     rew.write(','.join(map(str, x)))
     rew.write("\n")
 rew.close()
 tranFile = 'tdata1'
 rewFile = 'rdata1'
-R_min = -1
-R_max = 10
+R_min = mi
+R_max = ma
 delta = 0.00001
