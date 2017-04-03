c0b079eb3e4999597605bba4187a73403fc44602
Tarun Gupta <tarun1995gupta@gmail.com>
2017-04-03 23:01:42 +0530

    Added V3

--- config.py
+++ config.py
@@ -1,25 +1,25 @@
 #flag=1 fileread
 flag = 1
-agents = 2
+agents = 3
 collectTimes = []
 transitTimes = []
 T = [8]*agents
 nloc = [3]*agents
-shared = [[0,1],[0,1],[0,1]]
+shared = [[0,1,2],[0,1,2],[0,1,2]]
 nc = len(shared)
 creward = [1,2,3]
 theta = 0.00001
 collectTimes.append([4]*nloc[0])
 collectTimes.append([8]*nloc[1])
-#collectTimes.append([8]*nloc[2])
+collectTimes.append([8]*nloc[2])
 transitTimes.append([[4]*nloc[0]]*nloc[0])
 transitTimes.append([[8]*nloc[1]]*nloc[1])
-#transitTimes.append([[8]*nloc[2]]*nloc[2])
+transitTimes.append([[8]*nloc[2]]*nloc[2])
 gamma = 0.8
 rewardCollection = []
 rewardCollection.append([1,3,7])
 rewardCollection.append([4,3,6])
-#rewardCollection.append([3,4,2])
+rewardCollection.append([3,4,2])
 alpha = 0.8
 delta = 0.000001
 R_min = min(creward)
@@ -33,6 +33,6 @@ for i in xrange(0, agents):
     mm = max(rewardCollection[i])
     if R_max < mm:
         R_max = mm
-R_max += 2.0
+R_max += 0.0
 print R_min, R_max
 #Rmax Rmin in normalizing cks
